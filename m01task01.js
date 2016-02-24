5f0537bf1d9ae91f16ad59951486ceacb423af85
patriotua14 <pirat-32@ukr.net>
2016-02-24 22:30:26 +0000

    jswork

--- m01task01.js
+++ m01task01.js
@@ -1,15 +1,58 @@
-﻿// Task01 - Write a code that returns any string value.
+// Task01 - Write a code that returns any string value.
 // For example, next code return a boolean value: 
 // return false;
   //a*b+c*d+e=g
-function task01(a, c, e, g) {
-    var j = e - g;
-    var dist = c*c - 4*j*a;
-    var d = (((-1*c) + Math.sqrt(dist))/2*a);
-    var q = (((-1*c) - Math.sqrt(dist))/2*a);
-    return  (q,d);  
-    
-    // TODO: Write your code here
+    function question (a,b,c) {
+    	var discriminant = b*b - 4*a*c;
+        if (discriminant < 0){ 
+      	return 'error';
+        }
+     		if (discriminant == 0 ) {
+     	    var u = (-b)/2*a;
+    	    return u;
+  	    	}
+   		    	 else {
+   		    	 var u = (-b + Math.sqrt(discriminant))/2*a;
+    		     var v = (-b - Math.sqrt(discriminant))/2*a;
+   		         return [u,v];
+ 			     }
+
 }
-var result = task01(1,4,0,9/4)
-console.log(result)
+console.log( question(1,int,3))
+
+    var i;
+    for( i = 0; i<10; i++) {
+    console.log(i)
+    }
+
+        var obj = {
+        name: 'Pavlo',
+        age:  '19',
+        student: 'Yes',
+        car: 'No'
+        };
+
+            var guy;
+            for( guy in obj) {
+            console.log( guy + ' : ' + obj[guy])
+            }
+                var n = -20;
+                while (n<20) {
+                    console.log( n++ )
+                }
+                
+
+    var int = function (a,b,c) {
+        if (a >=0 && b>=0 && c == null){
+        var s = a*b;
+        return s;
+             } if (a >=0 && b>=0 && c>= 0)
+             {
+             var p = (1/2)*a*b*Math.sin(c);
+             return p;
+            }
+    }
+    console.log( int(1,2,30))
+ 
+ 
+ 
